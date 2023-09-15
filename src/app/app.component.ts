d46832cea693fa8dfd0e5700ccb92d1791eb3425
Paul-Ngigi <paulkush7777@gmail.com>
2023-09-15 17:20:22 +0300

    add initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/interfaces/product.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular_test';
+  products: IProduct[] = [];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.products = [
+      {
+        image: 'assets/panel_kit.svg',
+        name: 'Apowery Solar Panel Kit 12V Monacrystalline Battery Maintainer + 10A Solar Charger',
+        amount: 180255,
+        color: 'Black',
+        available: true,
+        totalItems: 2,
+      },
+      {
+        image: 'assets/panel_kit.svg',
+        name: 'Apowery Solar Panel Kit 12V Monacrystalline Battery Maintainer + 10A Solar Charger',
+        amount: 180255,
+        color: 'Black',
+        available: true,
+        totalItems: 2,
+      },
+      {
+        image: 'assets/panel_kit.svg',
+        name: 'Apowery Solar Panel Kit 12V Monacrystalline Battery Maintainer + 10A Solar Charger',
+        amount: 180255,
+        color: 'Black',
+        available: true,
+        totalItems: 2,
+      },
+    ];
+  }
+
+  subtract(idx: number) {
+    if (this.products[idx]?.totalItems) {
+      this.products[idx].totalItems--;
+    }
+  }
+
+  add(idx: number) {
+    if (this.products[idx]?.totalItems) {
+      this.products[idx].totalItems++;
+    }
+  }
+
+  delete(idx: number) {
+    if (idx >= 0 && idx < this.products.length) {
+      this.products.splice(idx, 1);
+    }
+  }
 }
---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  image: string;
+  name: string;
+  amount: number;
+  color: string;
+  available: boolean;
+  totalItems: number;
+}
